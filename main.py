825121bf015151f21f8ddc3c75019513298bd597
Stas <bihug95@gmail.com>
2021-03-14 16:43:12 +0300

    Решение для 3 задачи flask-sqlalchemy 2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask import Flask, render_template, redirect
 from forms.user import RegisterForm
 from forms.login import LoginForm
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -18,7 +19,7 @@ def main():
 
 @app.route('/')
 def index():
-    return render_template('base.html', title="Главная страница")
+    return render_template('index.html', title="Главная страница")
 
 
 @app.route('/register', methods=['GET', 'POST'])
