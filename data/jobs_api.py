facb8db659a117e783726464f5575ab9a03e251b
Stas <bihug95@gmail.com>
2021-03-23 17:07:38 +0300

    flask-rest_api class 3.1

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 
 from . import db_session
 from .jobs import Jobs
@@ -18,7 +18,7 @@ def get_news():
     return jsonify(
         {
             'jobs':
-                [item.to_dict(only=('user.id', 'team_leader', 'job', 'work_size',
+                [item.to_dict(only=('id', 'user.id', 'team_leader', 'job', 'work_size',
                                     'collaborators', 'is_finished', 'start_date', 'end_date'))
                  for item in jobs]
         }
@@ -28,12 +28,47 @@ def get_news():
 @blueprint.route('/api/jobs/<int:jobs_id>', methods=['GET'])
 def get_one_news(jobs_id):
     db_sess = db_session.create_session()
-    news = db_sess.query(Jobs).get(jobs_id)
-    if not news:
+    action = db_sess.query(Jobs).get(jobs_id)
+    if not action:
         return jsonify({'error': 'Not found'})
     return jsonify(
         {
-            'job': news.to_dict(only=('user.id', 'team_leader', 'job', 'work_size',
-                                      'collaborators', 'is_finished', 'start_date', 'end_date'))
+            'job': action.to_dict(only=('id', 'user.id', 'team_leader', 'job', 'work_size',
+                                        'collaborators', 'is_finished', 'start_date', 'end_date'))
         }
     )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_jobs():
+    print(request.json)
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['team_leader', 'job', 'work_size', 'collaborators', 'is_finished']):
+        return jsonify({'error': 'Bad request'})
+
+    db_sess = db_session.create_session()
+    if db_sess.query(Jobs).filter(Jobs.id == request.json['id']).all():
+        return jsonify({'error': 'id already exists'})
+    jobs = Jobs(
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        is_finished=request.json['is_finished']
+    )
+    db_sess.add(jobs)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['DELETE'])
+def delete_jobs(jobs_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(jobs_id)
+    if not jobs:
+        return jsonify({'error': 'Not found'})
+    db_sess.delete(jobs)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
--- data/test.py
+++ data/test.py
@@ -1,6 +1,20 @@
-from requests import get
+from requests import get, post, delete
+from pprint import pprint
 
-print(get('http://localhost:5000/api/jobs').json())
-print(get('http://localhost:5000/api/jobs/1').json())
-print(get('http://localhost:5000/api/jobs/99999').json())
-print(get('http://localhost:5000/api/jobs/q').json())
+job = {
+    'id': 2,
+    'team_leader': 1,
+    'job': 'Образец',
+    'work_size': 15,
+    'collaborators': '2 and 3',
+    'is_finished': True
+}
+headers = {'Content-type': 'application/json'}
+
+print(post('http://localhost:5000/api/jobs', json=job).json())
+print(post('http://localhost:5000/api/jobs', json=job).json())
+
+print(delete('http://localhost:5000/api/jobs/999').json())
+print(delete('http://localhost:5000/api/jobs/1').json())
+
+pprint(get('http://localhost:5000/api/jobs').json())
